22e3ca286b64654a51e8e6429dbd1595d490b9d3
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-06-22 20:15:10 -0300

    Mudando nomes de variaveis e melhorando o codigo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/script.js
@@ -1,7 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let media = 0;
+let soma = 0;
 
 for (let index = 0; index < numbers.length; index+=1) {
-    media += numbers[index];  
+    soma += numbers[index]; 
+    media = soma / 10;
 }
-console.log("Média é igual: " + media / 10);
+console.log("Média é igual: " + media);
+
