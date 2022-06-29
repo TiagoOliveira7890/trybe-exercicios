9169b7ba538dfda8dff87bbf670005fb482de0fa
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-06-29 10:04:04 -0300

    Atualizando exercicio 1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicio-1/script.js
@@ -0,0 +1,9 @@
+document.getElementById("page-title").innerText = "Sem Limites";
+document.getElementById("page-title").style.background = "red";
+document.getElementById("page-title").style.color = "white";
+
+document.getElementById("paragraph").innerText = "Sinopse: Encarando o desemprego e a rejeição de sua namorada, o escritor Eddie Morra tem certeza que não tem mais futuro. Mas tudo muda quando um velho amigo lhe dá uma droga que melhora as habilidades mentais. Viciado nesta química ainda não testada, Eddie chega ao topo do mundo financeiro e chama a atenção de um magnata que pretende usá-lo para fazer fortuna. Mas terríveis efeitos colaterais e a diminuição do estoque da droga ameaçam o colapso de Eddie.";
+
+document.getElementById("subtitle").innerText = "Meu filme favorito";
+
+document.getElementById("second-paragraph").innerText = "Esse filme é muito bom! Assista!!!";
