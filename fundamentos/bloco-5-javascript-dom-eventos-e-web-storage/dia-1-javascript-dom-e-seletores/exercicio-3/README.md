b956a1a76b2cd49870e625d9a5270bd105f9ec12
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-06-29 10:48:33 -0300

    Finalizando Exercicio 3

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicio-3/README.md
@@ -0,0 +1,1 @@
+Exercicio Parte III- Qual seletor usar?
---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicio-3/script.js
@@ -0,0 +1,19 @@
+document.getElementById("header-container").style.background = "green";
+
+document.getElementsByClassName("emergency-tasks")[0].style.background = "pink";
+
+document.getElementsByClassName("no-emergency-tasks")[0].style.background = "yellow";
+
+document.querySelectorAll(".emergency-tasks h3")[0].style.background = "purple";
+document.querySelectorAll(".emergency-tasks h3")[1].style.background = "purple";
+
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.background = "black";
+
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.background = "black";
+
+
+
+
+
+
+
